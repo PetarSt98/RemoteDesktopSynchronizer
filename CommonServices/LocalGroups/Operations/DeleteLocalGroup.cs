c2bf2c98dff7a62686f6d05fa036ea3057225bf8
pstojkov <petar.stojkovic@cern.ch>
2024-03-18 15:08:23 +0100

    Cleaned Config file

--- CommonServices/LocalGroups/Operations/DeleteLocalGroup.cs
+++ CommonServices/LocalGroups/Operations/DeleteLocalGroup.cs
@@ -30,7 +30,7 @@ namespace SynchronizerLibrary.CommonServices.LocalGroups.Operations
                 LoggerSingleton.SynchronizedLocalGroups.Info($"Local Group successfully deleted {localGroup} on server {server}");
             else
                 LoggerSingleton.SynchronizedLocalGroups.Error($"Local Group unsuccessfully deleted {localGroup} on server {server}");
-            if (! await lapsService.SyncLAPS(server, localGroup))
+            if (!await lapsService.SyncLAPS(server, localGroup))
                 LoggerSingleton.SynchronizedLocalGroups.Info($"Local Group successfully deleted {localGroup} on device");
             else
                 LoggerSingleton.SynchronizedLocalGroups.Error($"Local Group unsuccessfully deleted {localGroup} on device");
@@ -48,7 +48,7 @@ namespace SynchronizerLibrary.CommonServices.LocalGroups.Operations
             try
             {
                 //var ad = new DirectoryEntry($"WinNT://CERN,computer", username, password);
-                var ad = new DirectoryEntry($"WinNT://CERN,computer");
+                var ad = new DirectoryEntry($"WinNT://{server},computer");
                 try
                 {
 
@@ -57,7 +57,7 @@ namespace SynchronizerLibrary.CommonServices.LocalGroups.Operations
                 }
                 catch (System.Runtime.InteropServices.COMException ex)
                 {
-                    if (ex.ErrorCode == -2147022675) // Group not found.
+                    if (ex.ErrorCode == -2147022675 || ex.Message.Contains("The group name could not be found")) // Group not found.
                     {
                         groupExists = false;
                     }
